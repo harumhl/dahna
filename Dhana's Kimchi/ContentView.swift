a4c7c3f16080812e2217a5b450c5e68de6e0ac31
Haru Lee <haru.lee@nmrk.com>
2022-11-24 11:22:14 -0600

    Hello World

---
+++ Dhana's Kimchi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dhana's Kimchi
+//
+//  Created by Haru Lee on 11/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dhana's Kimchi/Dhana_s_KimchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dhana_s_KimchiApp.swift
+//  Dhana's Kimchi
+//
+//  Created by Haru Lee on 11/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Dhana_s_KimchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
