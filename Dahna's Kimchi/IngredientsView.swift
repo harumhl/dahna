899794b83cf1679e6cd62fbb097f249b195693af
Haru Lee <haru.lee@nmrk.com>
2023-02-24 15:45:34 -0600

    image padding and text cases

--- Dahna's Kimchi/IngredientsView.swift
+++ Dahna's Kimchi/IngredientsView.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 let ingredientList = ["NapaCabbage", "BellPepper", "Radish"]
+func pascalCaseToSpaced(string: String) -> String {
+    let regexPattern = "[A-Z-_&](?=[a-z0-9]+)|[A-Z-_&]+(?![a-z0-9])"
+    return string.replacingOccurrences(of: regexPattern, with: " $0", options: .regularExpression, range: nil)
+}
 
 struct PhotosView: View {
     var body: some View {
         ForEach(ingredientList, id: \.self) {
+            Text(pascalCaseToSpaced(string: $0))
             Image($0)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
+                .padding()
         }
     }
 }
